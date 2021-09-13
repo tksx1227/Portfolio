dc0575c13cd48cc4ad6da71769d85c9763dda92c
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-09-13 15:17:24 +0900

    [add] 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+import { Layout } from "../components/Layout";
+
+const Custom404 = () => {
+  return (
+    <>
+      <Head>
+        <title>tksx1227 - 404</title>
+        <meta name="description" content="Page Not Found" />
+      </Head>
+      <Layout>
+        <div className={"text-center"}>
+          <h2 className={"text-2xl font-bold tracking-wider lg:text-3xl mb-8"}>
+            404エラー
+          </h2>
+          <p>お探しのページは見つかりませんでした。</p>
+          <p>再度URLをご確認ください。</p>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Custom404;
